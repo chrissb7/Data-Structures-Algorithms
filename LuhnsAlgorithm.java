6563319535a72bbfde3612ba86f77dbb5759c837
chrissb7 <74411730+chrissb7@users.noreply.github.com>
2021-09-29 22:56:31 +0100

    Update LuhnsAlgorithm.java
    Also known as the “mod 10” algorithm, a checksum formula used to validate credit card numbers​
    
    From the rightmost digit, which is the check digit, and moving left, double the value of every second digit. If the result of this doubling operation is greater than 9 (e.g., 8 × 2 = 16), then add the digits of the product (e.g., 16: 1 + 6 = 7, 18: 1 + 8 = 9) or alternatively subtract 9 from the product (e.g., 16: 16 - 9 = 7, 18: 18 - 9 = 9).​
    
    Take the sum of all the digits.​
    
    If the total modulo 10 is equal to 0 (if the total ends in zero) then the number is valid according to the Luhn formula; else it is not valid.

--- LuhnsAlgorithm.java
+++ LuhnsAlgorithm.java
@@ -1,3 +1,18 @@
+/* Also known as the “mod 10” algorithm, a checksum formula used to validate credit card numbers
+
+From the rightmost digit, which is the check digit, and moving left, double the value of every second digit. 
+
+If the result of this doubling operation is greater than 9 (e.g., 8 × 2 = 16), then add the digits of the product (e.g., 16: 1 + 6 = 7, 18: 1 + 8 = 9) 
+or alternatively subtract 9 from the product (e.g., 16: 16 - 9 = 7, 18: 18 - 9 = 9).
+
+Take the sum of all the digits.
+
+If the total modulo 10 is equal to 0 (if the total ends in zero) then the number is valid according to the Luhn formula; else it is not valid. */
+
+
+
+
+
 public class LuhnsAlgorithm {
 
     public static void main(String[] args) {
